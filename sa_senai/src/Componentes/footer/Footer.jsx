5051cd291efe8cd079d8e9095c1c9ea0f040f718
Renan Nunes <renan.s.nunes@aluno.senai.br>
2025-05-19 09:17:57 -0300

    footer finalizado

--- sa_senai/src/Componentes/footer/Footer.jsx
+++ sa_senai/src/Componentes/footer/Footer.jsx
@@ -9,6 +9,8 @@ import instaIcon from "../../assets/icones/instaicon.png"
 import ytIcon from "../../assets/icones/yticon.png"
 import faceIcon from "../../assets/icones/faceicon.png"
 
+import { Link } from "react-router-dom";
+
 const Footer = () => {
   return (
     <footer
@@ -48,23 +50,45 @@ const Footer = () => {
                     <Col className="d-flex justify-content-center">
                         <Row className="m-auto">
                             <Col>
-                                <img src={instaIcon} alt="" width="30px" className="mx-3"/>
-                                <img src={faceIcon} alt="" width="30px" className="mx-3" />
-                                <img src={ytIcon} alt="" width="30px" className="mx-3" />
+                                <Link to="https://www.instagram.com/senaies/" target="blank">
+                                    <img src={instaIcon} alt="" width="30px" className="mx-3" />
+                                </Link>
+                                <Link to="https://www.facebook.com/SenaiES/" target="blank">
+                                    <img src={faceIcon} alt="" width="30px" className="mx-3" />
+                                </Link>
+                                <Link to="https://www.youtube.com/c/SENAIES_Oficial" target="blank">
+                                    <img src={ytIcon} alt="" width="30px" className="mx-3" />
+                                </Link>
                             </Col>
                             
                         </Row>
                     </Col>
                 </Row>
             
-                <Row className="mt-3 text-center">
+                <Row className="mt-3 mb-2 text-center">
                     <Col>
                         <Card.Subtitle>
                             SAC - Serviço de Atendimento ao Cliente
                         </Card.Subtitle>
                     </Col>
                 </Row>
-
+                <Row style={{font:"normal 90% arial"}} className="p-1 mt-3">
+                    <Col xs={6} sm={4} className="mb-2">Telefone: 800-102-0880</Col>
+                    <Col xs={6} sm={4} className="mb-2">Whatsapp: 27 99841-2270 </Col>
+                    <Col xs={12} sm={4}>
+                        <Row className="mb-2">
+                            <Col>
+                                Segunda a Sexta, das 7 horas às 19h
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col>
+                                Exceto feriados nacionais e internacionais
+                            </Col>
+                        </Row>
+                    </Col>
+                    
+                </Row>
                
 
             </Card.Footer>
